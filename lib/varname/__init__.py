431e76be01314014b929a7f2e17771c42b2f967f
root <root@save20-ue1wcd1.internal.ptcmsdev.com>
2024-06-10 11:41:32 +0000

    updated

---
+++ lib/varname/__init__.py
@@ -0,0 +1,16 @@
+"""Dark magics about variable names in python"""
+
+from .utils import (
+    config,
+    VarnameException,
+    VarnameRetrievingError,
+    ImproperUseError,
+    QualnameNonUniqueError,
+    VarnameWarning,
+    MultiTargetAssignmentWarning,
+    MaybeDecoratedFunctionWarning,
+    UsingExecWarning,
+)
+from .core import varname, nameof, will, argname
+
+__version__ = "0.13.1"
